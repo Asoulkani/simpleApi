363f032af0b735121cba12348413dee86b5dd153
asoulkani <a.soulkani@gmail.com>
2026-08-28 19:43:00 +0100

    message from the properties

--- src/main/java/ma/aso/simpleapi/controller/Controller.java
+++ src/main/java/ma/aso/simpleapi/controller/Controller.java
@@ -1,5 +1,6 @@
 package ma.aso.simpleapi.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class Controller {
+    @Value("${helloMessage}")
+    String message;
 
     @GetMapping("v1/hello")
     public Response hello() {
-        return new Response("hello");
+        return new Response(message);
     }
 }
